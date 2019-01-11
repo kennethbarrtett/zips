9116c65c3b6dd5733610f8d1706261db9645cec2
kennethbarrtett <44135632+kennethbarrtett@users.noreply.github.com>
2019-01-11 13:42:16 -0500

    Update main.py

--- zips/main.py
+++ zips/main.py
@@ -18,7 +18,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.redirect("http://clicktrkr.xyz/1wqfry", True)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
